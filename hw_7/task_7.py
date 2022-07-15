6fa6ecf2e368fb30787406f194007a505fc53942
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-07-15 22:21:05 +0300

    Создание task_8 и добавление новых документов

---
+++ hw_7/task_7.py
@@ -0,0 +1,20 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt 
+# в одной строке одно число.
+import random
+
+n = int(input('Введите любое число и нажмите "Enter": '))
+file = open('text.txt', 'w')
+ind = random.randint(0,n)
+file.write(str(ind)+'\n')
+file.write(str(random.randint(0,n)))
+string = []
+for i in range (n):
+    string.append(random.randint (-n, n))
+result = 1
+with open('text.txt', 'r') as f:
+    result *= string[int(f.read())]
+print (string)
+print(result)
+
+file.close()
