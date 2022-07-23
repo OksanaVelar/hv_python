7f8f97471d577ea2d3197a4af6f3513278ec0ecc
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-07-24 02:54:11 +0300

    Создан код программы

--- hv_11/task_2.py
+++ hv_11/task_2.py
@@ -1,9 +1,15 @@
 # Задайте натуральное число N. Напишите программу, 
 # которая составит список простых множителей числа N
+
 n = int(input('Введите любое натуральное число и нажмите "Enter": '))
-shed = []
-def Prime_num(n):
-    d = 2
-    while n % d != 0:
-        d += 1
-        return d == n
+
+for i in range (n-1, 1, -1):
+    prime = 0
+    if(n % i == 0):
+        for j in range(i-1, 1, -1):
+            if(i % j == 0):
+                prime += 1
+        if(prime == 0):
+            print (i)
+
+
