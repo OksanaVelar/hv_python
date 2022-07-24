2e80b2e34cdbca19cdfce8a1d89831c7d5f4bd25
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-07-24 16:39:45 +0300

    Скорректирован код - добавлен метод

--- hv_11/task_2.py
+++ hv_11/task_2.py
@@ -1,8 +1,7 @@
 # Задайте натуральное число N. Напишите программу, 
 # которая составит список простых множителей числа N
-
 n = int(input('Введите любое натуральное число и нажмите "Enter": '))
-
+shed = []
 for i in range (n-1, 1, -1):
     prime = 0
     if(n % i == 0):
@@ -10,6 +9,31 @@ for i in range (n-1, 1, -1):
             if(i % j == 0):
                 prime += 1
         if(prime == 0):
-            print (i)
+            shed.append(i)
+def Print_list(my_list):
+    for i in range (len(my_list)):
+        if i == len(my_list) -1:
+            print (f'{my_list[i]}.')
+        else:
+            print(f'{my_list[i]},', end = '')
+
+print(f'Простые множители числа {n}:', end = ' ')
+Print_list(shed)
+
+
+
+
+# n = int(input('Введите любое натуральное число и нажмите "Enter": '))
+# shed = []
+# for i in range (n-1, 1, -1):
+#     prime = 0
+#     if(n % i == 0):
+#         for j in range(i-1, 1, -1):
+#             if(i % j == 0):
+#                 prime += 1
+#         if(prime == 0):
+#             shed.append(i)
+# print(f'Простые множители числа {n} : {shed}')
+#             # print (i, end = ', ')
 
 
