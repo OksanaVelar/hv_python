02ffb3fd3ab372d37f2aaed1bcca0caff797216a
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-07-24 21:12:48 +0300

    Удалены неактуальные комментарии

--- hv_11/task_3.py
+++ hv_11/task_3.py
@@ -1,20 +1,6 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет 
 # список неповторяющихся элементов исходной последовательности
-# n = int(input('Введите максимальное число последовательности и нажмите "Enter": '))
-# orshed = [1,8,3,9,5,6,0,9,4,4,4,1]
-# print(orshed)
-# dup = None
-# checknum = None
-# next = None
 
-# for i in range(len(orshed)):
-#     checknum = orshed[i]
-#     for j in range(len(orshed)-1):
-#         next = orshed[j+1]
-#         if checknum == next:
-#             dup == next
-#             orshed.pop(dup)
-        
 orshed = [1,8,3,9,5,6,0,9,4,4,4,1]
 print(f'Исходная последовательность: {orshed}')
 reshed =[]
@@ -24,15 +10,3 @@ for i in orshed:
 
 # print(reshed)
 print(f'Измененная последовательность: {reshed}')
-
-
-# def Print_list(shed):
-#     for i in range (len(shed)):
-#         if i == len(shed)-1:
-#             print(f'{shed[i]}.')
-#         else:
-#             print(f'{shed[i]},', end = '')
-
-# Print_list(result)
-
-
