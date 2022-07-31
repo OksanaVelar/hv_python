15c91cc51e9bda0fdec9b125653627854e961ecd
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-07-31 18:42:58 +0300

    Добавлен код для бота

--- hv_12/task_3.py
+++ hv_12/task_3.py
@@ -10,21 +10,27 @@
 
 import random
 
-sweets = 106
+sweets = 100
 max = 28
-Gamershed = ['Gmr1','Gmr2']
+gamershed = ['Gmr1','Gmr2']
 winner = None
+game = True
 print(f'Список игроков: \n   Gmr1\n   Gmr2')
 first_stp = (input('Для проведения жеребьевки нажмите клавишу "Enter". Жеребьевка будет осуществлена автоматически:\n'))
+# currgmr = None
 
-for i in range (len(Gamershed)):
-    first_gamer = random.choice(Gamershed)
+for i in range (len(gamershed)):
+    first_gamer = random.choice(gamershed)
+    # currgmr = first_stp
 print(f'Право первого хода предоставляется игроку {first_gamer}')
 
 def Show_winner(shed):
     for i in range (len(shed)):
-        # shed.reverse()
-        winner = shed[i-1]
+        if game == True:
+            winner = shed[i-1]
+        elif game == False:
+            # shed.reverse()
+            winner = shed[i]
     return(winner)
 
 def sub(x,y):
@@ -36,13 +42,70 @@ while sweets > max:
     if number <= max and number != 0:
         sweets = int(sub(sweets,number))
         print(f'Осталось конфет: {sweets}\n')
-        winner = Show_winner(Gamershed)
+        winner = Show_winner(gamershed)
     elif number == 0:
+        game = False
+        winner = Show_winner(gamershed)
         print('Ход не выполнен. Для выполнения хода необходимо ввести число не менее 1')
     elif number > max:
+        game = False
+        winner = Show_winner(gamershed)
         print('Ход не выполнен. Для выполнения хода необходимо ввести число, не превышающее остаток')
 print(f'Победитель - {winner}')
 
+
+# КОД ДЛЯ БОТА:
+# sweets = 100
+# max = int(28)
+# gamershed = ['Gmr1','mach']
+# winner = None
+# game = True
+# # autostep = None
+# print(f'Список игроков: \n   Gmr1\n   mach')
+# first_stp = (input('Для проведения жеребьевки нажмите клавишу "Enter". Жеребьевка будет осуществлена автоматически:\n'))
+
+# for i in range (len(gamershed)):
+#     first_gamer = random.choice(gamershed)
+#     # currgmr = first_stp
+# print(f'Право первого хода предоставляется игроку {first_gamer}')
+
+# def Choose_step (gamershed):
+#     for i in gamershed:
+#         if first_gamer == gamershed[i+1]:
+#             step = int(random.randint(1,29)) # autostep 
+#         elif first_gamer == gamershed[i]:
+#             step = int(input('Выполните ход - введите число от 1 до 28, не превышающее остаток и нажмите "Enter"\n ')) # humanstep
+#     return(step)    
+
+# def Show_winner(shed):
+#     for i in range (len(shed)):
+#         if game == True:
+#             winner = shed[i-1]
+#         elif game == False:
+#             # shed.reverse()
+#             winner = shed[i]
+#     return(winner)
+
+# def sub(x,y):
+#     balance = x-y
+#     return(balance)
+
+# while sweets > max:
+#     step = Choose_step(gamershed)
+#     print (step)
+#     if step <= max and step != 0:
+#         sweets = int(sub(sweets,step))
+#         print(f'Осталось конфет: {sweets}\n')
+#         winner = Show_winner(gamershed)
+#     elif step == 0:
+#         game = False
+#         winner = Show_winner(gamershed)
+#         print('Ход не выполнен. Для выполнения хода необходимо ввести число не менее 1')
+#     elif step > max:
+#         game = False
+#         winner = Show_winner(gamershed)
+#         print('Ход не выполнен. Для выполнения хода необходимо ввести число, не превышающее остаток')
+# print(f'Победитель - {winner}')
 
 
 
