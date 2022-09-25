fc1f6102553aa30c166f29d93ff01e6208944a08
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-09-25 14:23:09 +0300

    Прописан код сжатия

--- hv_12/RLE.py
+++ hv_12/RLE.py
@@ -1,46 +1,30 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных
 
-# Создание нового элемента
-def Get_new_symb(number, symb):
-    new_symb = '%d%s' % (number, symb)
-    return (new_symb)
-
-# замена элемента в исходной строке
-def Replace_sym (str, symb, sym2):
-    new_str = str.replace(symb, sym2)
-    return(new_str)
-
-def Join_new_symb(symb1,symb2):
-    grow_str = symb1+symb2
-    return(grow_str)
-# encoded_str = Join_part_str(part1,part2)
-# print(encoded_str)
-
-init_str = 'дистиллированный'
-num = 1
-for i in range (len(init_str)):
-    new_symb = Get_new_symb(num, init_str[i])
-    new_str = Replace_sym (init_str, init_str[i], new_symb)
+# Формирование длины среза
+def Cut_len(spsk):
+    cut_len = spsk[:-1]
+    return cut_len
+    
+# Создание закодированной строки
+def Uncode_parts(stroka):
+    i = 0
+    count = 1
+    new_stroka = ''   
+    for i in range (len(stroka)-1):
         
-    print(new_str)
+        if stroka[i]==stroka[i+1]:
+            count+=1
         
-    # elif init_str[i] == init_str[i+1]:
-    #     num = num+1
-    #     new_symb = Get_new_symb(num, init_str[i])
-    #     uncoded_str = init_str.replace(init_str[i], new_symb)
-    #     uncoded_str = init_str.replace(init_str[i+1], '')
-   
+        else:
+            new_stroka += '%d%s' % (count,stroka[i])
+            count = 1
 
+        if i == (len(stroka)-2):
+            new_stroka += '%d%s' % (count,stroka[i])
+    
+    return new_stroka
+    
+original = 'AAAytttt'
+uncoded = Uncode_parts(original)
+print (uncoded)
 
-# print(uncoded_str)
-
-
-# Соединение пар в закодированную строку
-# part1 = "4";
-# part2 = "8";
-
-# def Join_new_symb(symb):
-#     grow_str = symb+symb
-#     return(grow_str)
-# encoded_str = Join_part_str(part1,part2)
-# print(encoded_str)
