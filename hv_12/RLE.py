f0aff3e927a0d0ff7517c220a98cf1b5d34924a5
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-09-27 13:24:59 +0300

    Добавлен код на восстановление данных

--- hv_12/RLE.py
+++ hv_12/RLE.py
@@ -1,11 +1,9 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных
 
-# Формирование длины среза
-def Cut_len(spsk):
-    cut_len = spsk[:-1]
-    return cut_len
-    
-# Создание закодированной строки
+''' 
+    Создание закодированной строки
+'''
+
 def Uncode_parts(stroka):
     i = 0
     count = 1
@@ -15,16 +13,65 @@ def Uncode_parts(stroka):
         if stroka[i]==stroka[i+1]:
             count+=1
         
-        else:
-            new_stroka += '%d%s' % (count,stroka[i])
+        else: 
+            new_stroka += "%d%s," % (count,stroka[i])
             count = 1
 
         if i == (len(stroka)-2):
-            new_stroka += '%d%s' % (count,stroka[i])
+            new_stroka += "%d%s" % (count,stroka[i+1])
     
     return new_stroka
-    
-original = 'AAAytttt'
+
+# original = 'ФФОТТ'
+# original = 'иллюминатор'
+original = 'напрррррррррррррррррррра-во!'
+print()
+print(f'Исходная строка: \n {original}')
 uncoded = Uncode_parts(original)
-print (uncoded)
+print (f'Закодированная строка: \n {uncoded}')
+
+# разбиение строки по запятым
+def Get_Temp_Str(ST):
+    STR = ST.split(',')
+    return STR
+
+# выделение среза <индекс+символ>
+def Splt (t):
+    tt = t[i]
+    return tt
+
+# вывод элемента (последний символ строки)
+def Get_last(S):
+    return S[-1]
+
+# вывод индекса (все, что до последнего символа строки)
+def Get_Indx(e):
+    return e[:-1]
+
+# создание строки из заданной длины
+def Get_str (num, smbl):
+    longstr = ''
+    for i in range (int(num)):
+        longstr += smbl
+    return longstr
+
+
+back_str = ''
+s1 = Get_Temp_Str(uncoded) #разбиение строки по запятым
+# print(s1)
+
+for i in range (len(s1)):
+    sr = Splt (s1) # выделение среза <индекс+символ>
+    # print(sr) 
+    el = Get_last(sr) # вывод элемента (последний символ строки)
+    # print(el)
+    ind = Get_Indx(sr) # вывод индекса (все, что до последнего символа строки)
+    # print (ind)
+    lnst = Get_str(ind,el) # создание строки из заданной длины
+    # print(lnst)
+    back_str +=lnst
+    
+print(f'Восстановленная строка: \n {back_str}')
+
+
 
