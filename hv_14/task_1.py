269f68214633ea853bf27fa89456892cc5ec8f12
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2023-01-26 08:59:10 +0300

    Дополнен код для LAMBDA и LIST COMPREHENSION на строке 41

--- hv_14/task_1.py
+++ hv_14/task_1.py
@@ -33,3 +33,38 @@ for i in range (len(shed2)):
         sum += shed2[i]
 
 print(sum)
+
+# LAMBDA и LIST COMPREHENSION :
+# Задайте список из вещественных чисел. Напишите программу, 
+# которая найдёт разницу между максимальным и минимальным значениями 
+# дробной части элементов.
+shed = [1.1, 5.2, 7.9, 12.6]
+print(f'Массив вещественных чисел : {shed}')
+new_shed = [int(shed[i]*10)%10 for i in range (len(shed))] # LIST COMPREHENSION
+print(f'Массив дробных частей элементов вышеприведенного массива : {new_shed}')
+
+def Math(x,y):
+    return x-y
+
+for i in range(1,len(new_shed)):
+    num = new_shed[i]
+    if i == 1:
+        max = num
+    elif max < num:
+        max = num
+print(f'Максимальное значение дробной части = {max}')
+
+min = new_shed[0]
+size = len(new_shed)
+i = 0
+while (i < size):
+    if(min > new_shed[i]):
+        min = new_shed[i]
+    i+=1
+
+print(f'Минимальное значение дробной части = {min}')
+def calc(op, a,b):
+    return op(a,b)
+
+w = calc(lambda x, y: x - y, max, min) # LAMBDA
+print(f'Разность максимального и минимального значений = {w}')
