2544e8d5a26d0a3452fd82ffa4592f57fee4e859
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2023-01-27 16:43:32 +0300

    Добавлен код со строки 81

--- hv_14/task_1.py
+++ hv_14/task_1.py
@@ -1,7 +1,8 @@
-# LAMBDA:
-# напишите программу, которая найдёт произведение пары чисел списка. 
-# парой считаем первый и последний элемент.
-
+"""
+LAMBDA:
+напишите программу, которая найдёт произведение пары чисел списка. 
+парой считаем первый и последний элемент.
+"""
 # def calc(x, y):
 #     return x*y
 # f = calc
@@ -19,52 +20,105 @@
 # rez = f2(part1, part2, lambda part1, part2: part2*part1)
 
 # print (rez)
+"""
+LIST COMPREHENTION:
+Задайте список из нескольких чисел. Напишите программу, 
+которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+"""
+# shed = [1,5,7,3,73,59,345]
+# sum = 0
+# shed2 = [shed.pop(i) for i in range (len(shed)//2+1)] # LIST COMPREHENSION
+# print(shed2)
+# for i in range (len(shed2)):
+#     if i !=0:
+#         sum += shed2[i]
 
-# LIST COMPREHENSION:
-# Задайте список из нескольких чисел. Напишите программу, 
-# которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# print(sum)
+"""""
+LAMBDA и LIST COMPREHENTION :
+Задайте список из вещественных чисел. Напишите программу, 
+которая найдёт разницу между максимальным и минимальным значениями 
+дробной части элементов.
+"""
 
-shed = [1,5,7,3,73,59,345]
-sum = 0
-shed2 = [shed.pop(i) for i in range (len(shed)//2+1)] # LIST COMPREHENSION
-print(shed2)
-for i in range (len(shed2)):
-    if i !=0:
-        sum += shed2[i]
+# shed = [1.1, 5.2, 7.9, 12.6]
+# print(f'Массив вещественных чисел : {shed}')
+# new_shed = [int(shed[i]*10)%10 for i in range (len(shed))] # LIST COMPREHENSION
+# print(f'Массив дробных частей элементов вышеприведенного массива : {new_shed}')
 
-print(sum)
+# def Math(x,y):
+#     return x-y
 
-# LAMBDA и LIST COMPREHENSION :
-# Задайте список из вещественных чисел. Напишите программу, 
-# которая найдёт разницу между максимальным и минимальным значениями 
-# дробной части элементов.
-shed = [1.1, 5.2, 7.9, 12.6]
-print(f'Массив вещественных чисел : {shed}')
-new_shed = [int(shed[i]*10)%10 for i in range (len(shed))] # LIST COMPREHENSION
-print(f'Массив дробных частей элементов вышеприведенного массива : {new_shed}')
+# for i in range(1,len(new_shed)):
+#     num = new_shed[i]
+#     if i == 1:
+#         max = num
+#     elif max < num:
+#         max = num
+# print(f'Максимальное значение дробной части = {max}')
 
-def Math(x,y):
-    return x-y
+# min = new_shed[0]
+# size = len(new_shed)
+# i = 0
+# while (i < size):
+#     if(min > new_shed[i]):
+#         min = new_shed[i]
+#     i+=1
 
-for i in range(1,len(new_shed)):
-    num = new_shed[i]
-    if i == 1:
-        max = num
-    elif max < num:
-        max = num
-print(f'Максимальное значение дробной части = {max}')
+# print(f'Минимальное значение дробной части = {min}')
+# def calc(op, a,b):
+#     return op(a,b)
 
-min = new_shed[0]
-size = len(new_shed)
+# w = calc(lambda x, y: x - y, max, min) # LAMBDA
+# print(f'Разность максимального и минимального значений = {w}')
+
+"""
+LIST COMPREHENTION, LAMBDA, MAP
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 2D пространстве
+"""
+
+coord_A = []
+coord_B = []
+arr_points = []
 i = 0
-while (i < size):
-    if(min > new_shed[i]):
-        min = new_shed[i]
-    i+=1
+q = None
+q1 = None
 
-print(f'Минимальное значение дробной части = {min}')
-def calc(op, a,b):
-    return op(a,b)
+while i <= 3:
+    if i <= 1:
+        print('Введите значение координату "x" точки A и нажмите "Enter": ')
+        q = int(input())
+        coord_A.append(q)
+        i = i+1
+    else:
+        print('Введите значение координаты точки В и нажмите "Enter": ')
+        q1 = int(input())
+        coord_B.append(q1)
+        i = i+1
+print(f'Координаты точки A: {coord_A} \nКоординаты точки B: {coord_B}')
 
-w = calc(lambda x, y: x - y, max, min) # LAMBDA
-print(f'Разность максимального и минимального значений = {w}')
+def mathem (arr):
+    e = arr[0]-arr[1]
+    return e
+
+a = mathem(coord_A)
+b = mathem(coord_B)
+arr_points.append(a)
+arr_points.append(b)
+print (arr_points)
+
+def math2 (l):
+    return l**2
+
+square_A = list(map(math2, arr_points))
+print (square_A)
+
+import math
+def Sum (op, s,c):
+    t = op(s,c)
+    return t
+
+sum = Sum(lambda s,c: s+c, square_A[0], square_A[1])
+sqrt = math.sqrt(sum)
+print (f'Расстояние между заданными точками = {sqrt}')
